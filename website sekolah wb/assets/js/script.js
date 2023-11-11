b0da144251486b6648d5a7a0023192100b533f5b
ibraa07 <150199514+ibraa07@users.noreply.github.com>
2023-11-11 07:55:40 +0700

    Add files via upload

---
+++ website sekolah wb/assets/js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+ //element toggle function
+
+const toggleElem = function (elem) { elem.classList.toggle("active"); }
+
+
+
+//navbar toogle
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navTogglers.length; i++) {
+  navTogglers[i].addEventListener("click", function () {
+    toggleElem(navbar);
+    toggleElem(overlay);
+  });
+}
+
+//back to top
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    header.classList.add("header-anim");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+    header.classList.remove("header-anim");
+  }
+});
